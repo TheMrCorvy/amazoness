eb3d3c7ec7f30debfef5ea06e29a5366899047f8
Gonzalo Salvador Corvalan <mr.corvy@gmail.com>
2021-10-26 17:37:05 -0300

    created app theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,19 @@
 import "../styles/globals.css"
+
 import type { AppProps } from "next/app"
+
+import { ThemeProvider } from "@mui/material/styles"
+import theme from "../misc/theme"
 
 import Layout from "../components/Layout"
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
-		<Layout>
-			<Component {...pageProps} />
-		</Layout>
+		<ThemeProvider theme={theme}>
+			<Layout>
+				<Component {...pageProps} />
+			</Layout>
+		</ThemeProvider>
 	)
 }
 
