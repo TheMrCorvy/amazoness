566857183c3b03dc73c68499e8629bdd74bf3789
Gonzalo Salvador Corvalan <mr.corvy@gmail.com>
2021-10-28 08:58:19 -0300

    prepared the page to support api calls

--- src/pages/product/[slug].tsx
+++ src/pages/product/[slug].tsx
@@ -1,28 +1,64 @@
-import { FC } from "react"
+import { FC, useEffect, useState } from "react"
 
 import { useRouter } from "next/router"
 
 import { Container, Typography } from "@mui/material"
 
 import { data, urlKeyWords } from "../../misc/staticData"
-import { useSlug } from "../../components/utils"
+import { useFakeApi, useSlug } from "../../components/utils"
+import { Product, Req } from "../../misc/types"
 
 const ProductPage: FC = () => {
 	const router = useRouter()
 	const createSlug = useSlug
-	const { slug } = router.query
 
-	const product = data.products.find((element) => createSlug(element.name) === slug)
+	const [stateProduct, setStateProduct] = useState<Product>(placeholder)
 
-	if (!product) {
-		router.push(urlKeyWords.productNotFound)
-	}
+	useEffect(() => {
+		if (!router.isReady) return
+
+		const { slug } = router.query
+
+		const req: Req = {
+			endpoint: "/find",
+			method: "GET",
+		}
+
+		useFakeApi(req).then(() => {
+			let product: Product = placeholder
+
+			data.products.forEach((element) => {
+				if (createSlug(element.name) === slug) {
+					product = element
+				}
+			})
+
+			if (!product.id) {
+				router.push(urlKeyWords.productNotFound)
+			} else {
+				setStateProduct(product)
+			}
+		})
+	}, [router.isReady])
 
 	return (
 		<Container maxWidth="lg">
-			<Typography variant="h1">{product.name}</Typography>
+			<Typography variant="h1">{stateProduct.name}</Typography>
 		</Container>
 	)
+}
+
+const placeholder: Product = {
+	name: "loading",
+	id: 0,
+	category: "",
+	description: "",
+	images: [""],
+	price: 0,
+	brand: "",
+	rating: 0,
+	numReviews: 0,
+	stock: 0,
 }
 
 export default ProductPage
