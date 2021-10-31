b16201acaef8b5da8b82fadfe327162b6f5324d2
Gonzalo Salvador Corvalan <mr.corvy@gmail.com>
2021-10-31 20:11:53 -0300

    updated response

--- src/pages/api/products/index.ts
+++ src/pages/api/products/index.ts
@@ -2,13 +2,16 @@ import type { NextApiRequest, NextApiResponse } from "next"
 import Product from "../../../models/Product"
 
 import dbConnect from "../../../misc/db"
+import { data } from "../../../misc/staticData"
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
 	switch (req.method) {
 		case "GET":
 			const data = await getProducts()
 
-			return res.status(200).json({ data })
+			return res
+				.status(200)
+				.json({ status: 200, message: "products loaded successfully", data })
 
 		default:
 			return res.status(400).json({ msg: "This method is not supported", data: {} })
@@ -17,6 +20,9 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 
 const getProducts = async () => {
 	await dbConnect()
+
+	await Product.deleteMany()
+	await Product.insertMany(data.products)
 
 	const products = await Product.find()
 
