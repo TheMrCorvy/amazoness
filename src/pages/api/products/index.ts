eda4034b03d1e679d26a976ea69f0e33ec51b358
Gonzalo Salvador Corvalan <mr.corvy@gmail.com>
2021-11-01 17:56:28 -0300

    removed "seeder"

--- src/pages/api/products/index.ts
+++ src/pages/api/products/index.ts
@@ -2,7 +2,6 @@ import type { NextApiRequest, NextApiResponse } from "next"
 import Product from "../../../models/Product"
 
 import dbConnect from "../../../misc/db"
-import { data } from "../../../misc/staticData"
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
 	switch (req.method) {
@@ -20,9 +19,6 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 
 const getProducts = async () => {
 	await dbConnect()
-
-	await Product.deleteMany()
-	await Product.insertMany(data.products)
 
 	const products = await Product.find()
 
