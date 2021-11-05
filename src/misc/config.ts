c54d797e18fe53dbb4b43a0091a960f29875b1f8
Gonzalo Salvador Corvalan <mr.corvy@gmail.com>
2021-11-05 15:49:05 -0300

    added jwt config

--- src/misc/config.ts
+++ src/misc/config.ts
@@ -16,3 +16,10 @@ export const appName = "Amazoness"
 export const saltWorkFactor = 10
 
 export const mongoDbUri = process.env.MONGODB_URI
+
+export const jwtConfig = {
+	accessTokenTtl: 15, //15 minutes
+	refreshTokenTtl: 15000,
+	privateKey: process.env.JWT_SECRET,
+	publicKey: process.env.JWT_PUBLIC,
+}
