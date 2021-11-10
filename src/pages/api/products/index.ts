cc9f5a56c758531b4e984458be5b0872daea7131
Gonzalo Salvador Corvalan <mr.corvy@gmail.com>
2021-11-10 15:50:03 -0300

    updated msg => message

--- src/pages/api/products/index.ts
+++ src/pages/api/products/index.ts
@@ -13,7 +13,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 				.json({ status: 200, message: "products loaded successfully", data })
 
 		default:
-			return res.status(400).json({ msg: "This method is not supported", data: {} })
+			return res.status(400).json({ message: "This method is not supported", data: {} })
 	}
 }
 
