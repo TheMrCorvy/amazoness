4814d8cb325cfdeb592f62e18bdf6d8cfbc74cbe
Gonzalo Salvador Corvalan <mr.corvy@gmail.com>
2021-11-11 11:23:41 -0300

    forgot to add "disabled" prop

--- src/components/ValidatedInput.tsx
+++ src/components/ValidatedInput.tsx
@@ -50,6 +50,7 @@ const ValidatedInput: FC<Props> = (props) => {
 						helperText={helperText()}
 						id={input.id}
 						type={input.type}
+						disabled={input.disabled}
 						{...field}
 					/>
 				</FormControl>
