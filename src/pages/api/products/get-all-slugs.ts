1eb7c536f0c00bec4e2914cf0c94b50d7b646c46
Gonzalo Salvador Corvalán <mr.corvy@gmail.com>
2022-01-25 16:15:23 -0300

    created endpoint

---
+++ src/pages/api/products/get-all-slugs.ts
@@ -0,0 +1,26 @@
+import type { NextApiRequest, NextApiResponse } from "next"
+import Product from "../../../database/models/Product"
+
+import { dbConnect, dbDisconnect } from "../../../database"
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+	if (req.method !== "GET") {
+		return res
+			.status(400)
+			.json({ message: "This method is not supported", data: {}, status: 400 })
+	}
+
+	const data = await getProducts()
+
+	return res.status(200).json({ status: 200, message: "products loaded successfully", data })
+}
+
+const getProducts = async () => {
+	await dbConnect()
+
+	const products = await Product.find().select({ slug: 1 })
+
+	await dbDisconnect()
+
+	return { products }
+}
