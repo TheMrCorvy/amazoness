42bbe7de02249acf26957a9cd268b305947f7b4f
Gonzalo Salvador Corvalán <mr.corvy@gmail.com>
2022-01-25 14:57:49 -0300

    added ssr for index page

--- src/components/sections/PopularProducts/index.tsx
+++ src/components/sections/PopularProducts/index.tsx
@@ -1,39 +1,13 @@
-import { FC, useEffect, useState } from "react"
+import { FC } from "react"
 
 import { Container, Grid } from "@mui/material"
 
-import { useDispatch } from "react-redux"
-import { setErrorLoading } from "../../../redux/actions/loadingActions"
-import { useApi } from "../../utils"
-
-import { Product, Req } from "../../../misc/types"
+import { Product } from "../../../misc/types"
 
 import UnderlinedTitle from "../../UnderlinedTitle"
 import ProductCard from "../../ProductCard"
-import SkeletonProductCard from "../../SkeletonProductCard"
 
-const PopularProducts: FC = () => {
-	const callApi = useApi
-	const dispatch = useDispatch()
-
-	const [products, setProducts] = useState<Product[] | any[]>(["", "", "", ""])
-	const [loading, setLoading] = useState(true)
-
-	useEffect(() => {
-		const req: Req = {
-			endpoint: "/products/recently-added",
-			method: "GET",
-		}
-
-		callApi(req).then((res: any) => {
-			if (res.status === 200) {
-				setProducts(res.data.products)
-				setLoading(false)
-			} else {
-				dispatch(setErrorLoading(res.message))
-			}
-		})
-	}, [])
+const PopularProducts: FC<Props> = ({ products }) => {
 	return (
 		<Container maxWidth="lg">
 			<Grid container justifyContent="space-around" spacing={4}>
@@ -46,20 +20,18 @@ const PopularProducts: FC = () => {
 						variant="h4"
 					/>
 				</Grid>
-				{!loading
-					? products.map((product: Product, index: number) => (
-							<Grid item xs={12} md={6} lg={3} key={index}>
-								<ProductCard product={product} />
-							</Grid>
-					  ))
-					: products.map((product: Product, index: number) => (
-							<Grid item xs={12} md={6} lg={3} key={index}>
-								<SkeletonProductCard />
-							</Grid>
-					  ))}
+				{products.map((product: Product, index: number) => (
+					<Grid item xs={12} md={6} lg={3} key={index}>
+						<ProductCard product={product} />
+					</Grid>
+				))}
 			</Grid>
 		</Container>
 	)
+}
+
+interface Props {
+	products: Product[]
 }
 
 export default PopularProducts
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,38 @@
 import type { NextPage } from "next"
 
+import { useApi } from "../components/utils"
+
+import { Product, Req } from "../misc/types"
+
 import PopularProducts from "../components/sections/PopularProducts"
 
-const Home: NextPage = () => {
+const Home: NextPage<Props> = ({ recentlyAddedProducts }) => {
 	return (
 		<>
-			<PopularProducts />
+			<PopularProducts products={recentlyAddedProducts} />
 		</>
 	)
+}
+
+export async function getServerSideProps() {
+	const callApi = useApi
+
+	const req: Req = {
+		endpoint: "/products/recently-added",
+		method: "GET",
+	}
+
+	const recentlyAddedProducts = await callApi(req).then((res: any) => {
+		return res.data.products
+	})
+
+	return {
+		props: { recentlyAddedProducts },
+	}
+}
+
+interface Props {
+	recentlyAddedProducts: Product[]
 }
 
 export default Home
