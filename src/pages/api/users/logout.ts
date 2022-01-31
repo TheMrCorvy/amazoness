8c8c08d4cbd3d5b4587439766dac8ffb9dc9e0f3
Gonzalo Salvador Corvalán <mr.corvy@gmail.com>
2022-01-31 10:43:32 -0300

    deleted old login endo¡points

--- src/pages/api/users/logout.ts
+++
@@ -1,9 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from "next"
-
-export default async function handle(req: NextApiRequest, res: NextApiResponse) {
-	if (req.method !== "GET") {
-		return res
-			.status(400)
-			.json({ message: "This method is not supported", data: {}, status: 400 })
-	}
-}
