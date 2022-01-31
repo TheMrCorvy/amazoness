852431d929dc1db90d5963500e1ffb36f5eadd12
Gonzalo Salvador Corvalán <mr.corvy@gmail.com>
2022-01-31 10:46:28 -0300

    removed unused seeders

--- src/pages/api/admin/reset-db.ts
+++ src/pages/api/admin/reset-db.ts
@@ -1,11 +1,8 @@
 import type { NextApiRequest, NextApiResponse } from "next"
 
 import Product from "../../../database/models/Product"
-import Session from "../../../database/models/Session"
-import User from "../../../database/models/User"
 
 import productSeeds from "../../../database/seeders/productSeeder.json"
-import userSeeds from "../../../database/seeders/userSeeder.json"
 
 import { dbConnect, dbDisconnect } from "../../../database"
 
@@ -24,25 +21,10 @@ export default async function handle(req: NextApiRequest, res: NextApiResponse) 
 	console.log("Deleted all of the products.")
 	console.log("- - - - - - - - - - - - - - -")
 
-	await Session.deleteMany({})
-
-	console.log("Deleted all of the sessions.")
-	console.log("- - - - - - - - - - - - - - -")
-
-	await User.deleteMany({})
-
-	console.log("Deleted all of the users.")
-	console.log("- - - - - - - - - - - - - - -")
-
 	console.log("Starting to incert Data...")
 	console.log("- - - - - - - - - - - - - - -")
 
 	await Product.insertMany(productSeeds)
-
-	console.log("Incerted all products from seeder.")
-	console.log("- - - - - - - - - - - - - - -")
-
-	await User.insertMany(userSeeds)
 
 	console.log("Incerted all products from seeder.")
 	console.log("- - - - - - - - - - - - - - -")
