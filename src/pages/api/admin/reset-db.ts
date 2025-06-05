9274408bcdb5e70952d1eddf923053700494af7e
Gonzalo Salvador Corvalán <salvatore.hakase@gmail.com>
2025-06-05 17:11:19 -0300

    updated the migration endpoint

--- src/pages/api/admin/reset-db.ts
+++ src/pages/api/admin/reset-db.ts
@@ -1,55 +1,66 @@
-import type { NextApiRequest, NextApiResponse } from "next"
+import type { NextApiRequest, NextApiResponse } from "next";
 
-import Product from "../../../database/models/Product"
-import Session from "../../../database/models/Session"
-import User from "../../../database/models/User"
+import Product from "../../../database/models/Product";
+import Session from "../../../database/models/Session";
+import User from "../../../database/models/User";
 
-import productSeeds from "../../../database/seeders/productSeeder.json"
-import userSeeds from "../../../database/seeders/userSeeder.json"
+import productSeeds from "../../../database/seeders/productSeeder.json";
+import userSeeds from "../../../database/seeders/userSeeder.json";
 
-import { dbConnect, dbDisconnect } from "../../../database"
+import { dbConnect, dbDisconnect } from "../../../database";
 
-export default async function handle(req: NextApiRequest, res: NextApiResponse) {
-	if (req.method !== "GET") {
+export default async function handle(
+	req: NextApiRequest,
+	res: NextApiResponse
+) {
+	if (req.method !== "GET" || process.env.NODE_ENV === "production") {
 		return res
 			.status(400)
-			.json({ message: "This method is not supported", data: {}, status: 400 })
+			.json({
+				message: "This method is not supported",
+				data: {},
+				status: 400,
+			});
 	}
 
-	await dbConnect()
+	await dbConnect();
 
-	await Product.deleteMany({})
+	await Product.deleteMany({});
 
-	console.log("- - - - - - - - - - - - - - -")
-	console.log("Deleted all of the products.")
-	console.log("- - - - - - - - - - - - - - -")
+	console.log("- - - - - - - - - - - - - - -");
+	console.log("Deleted all of the products.");
+	console.log("- - - - - - - - - - - - - - -");
 
-	await Session.deleteMany({})
+	await Session.deleteMany({});
 
-	console.log("Deleted all of the sessions.")
-	console.log("- - - - - - - - - - - - - - -")
+	console.log("Deleted all of the sessions.");
+	console.log("- - - - - - - - - - - - - - -");
 
-	await User.deleteMany({})
+	await User.deleteMany({});
 
-	console.log("Deleted all of the users.")
-	console.log("- - - - - - - - - - - - - - -")
+	console.log("Deleted all of the users.");
+	console.log("- - - - - - - - - - - - - - -");
 
-	console.log("Starting to incert Data...")
-	console.log("- - - - - - - - - - - - - - -")
+	console.log("Starting to incert Data...");
+	console.log("- - - - - - - - - - - - - - -");
 
-	await Product.insertMany(productSeeds)
+	await Product.insertMany(productSeeds);
 
-	console.log("Incerted all products from seeder.")
-	console.log("- - - - - - - - - - - - - - -")
+	console.log("Incerted all products from seeder.");
+	console.log("- - - - - - - - - - - - - - -");
 
-	await User.insertMany(userSeeds)
+	await User.insertMany(userSeeds);
 
-	console.log("Incerted all products from seeder.")
-	console.log("- - - - - - - - - - - - - - -")
+	console.log("Incerted all products from seeder.");
+	console.log("- - - - - - - - - - - - - - -");
 
-	await dbDisconnect()
+	await dbDisconnect();
 
 	return res
 		.status(200)
-		.json({ message: "Database migrated and seeded successfully.", data: {}, status: 200 })
+		.json({
+			message: "Database migrated and seeded successfully.",
+			data: {},
+			status: 200,
+		});
 }
