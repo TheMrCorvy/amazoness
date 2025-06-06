9fb33bf207429ffa9e1f3f36200bf98e85d4acdf
Gonzalo Salvador Corvalán <salvatore.hakase@gmail.com>
2025-06-06 13:35:46 -0300

    added consolelog

--- src/pages/api/products/recently-added.ts
+++ src/pages/api/products/recently-added.ts
@@ -1,30 +1,43 @@
-import type { NextApiRequest, NextApiResponse } from "next"
-import Product from "../../../database/models/Product"
+import type { NextApiRequest, NextApiResponse } from "next";
+import Product from "../../../database/models/Product";
 
-import { dbConnect, dbDisconnect } from "../../../database"
+import { dbConnect, dbDisconnect } from "../../../database";
 
-export default async function handle(req: NextApiRequest, res: NextApiResponse) {
+export default async function handle(
+	req: NextApiRequest,
+	res: NextApiResponse
+) {
 	switch (req.method) {
 		case "GET":
-			const data = await getProducts()
+			const data = await getProducts();
+
+			console.log(data);
 
 			return res
 				.status(200)
-				.json({ status: 200, message: "products loaded successfully", data })
+				.json({
+					status: 200,
+					message: "products loaded successfully",
+					data,
+				});
 
 		default:
 			return res
 				.status(400)
-				.json({ message: "This method is not supported", data: {}, status: 400 })
+				.json({
+					message: "This method is not supported",
+					data: {},
+					status: 400,
+				});
 	}
 }
 
 const getProducts = async () => {
-	await dbConnect()
+	await dbConnect();
 
-	const products = await Product.find().sort({ _id: -1 }).limit(5)
+	const products = await Product.find().sort({ _id: -1 }).limit(5);
 
-	await dbDisconnect()
+	await dbDisconnect();
 
-	return { products }
-}
+	return { products };
+};
